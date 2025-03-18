9076edb52737fc1b42e77c78646f94b78c21c655
Stephen Abongo <stephenabongo48@gmail.com>
2025-03-18 08:19:08 -0700

    Created Register and Fixed bugs

--- stevemind-mental-health-app/src/pages/Register.jsx
+++ stevemind-mental-health-app/src/pages/Register.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { yupResolver } from "@hookform/resolvers/yup"
+import {useForm} from "react-hook-form"
+import *as yup from "yup"
+import { Link } from "react-router-dom"
 
-function Register() {
-  return (
-    <div>Register</div>
+
+const schema = yup.object() .shape({
+  fullName: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().min(8).max(12).required()
+})
+
+const Register =() => {
+  const {register, handleSubmit, formState: {errors}} =useForm({
+    resolver: yupResolver(schema)
+  })
+
+  const onSubmit =(data) => {
+    console.log("Congrats! You've been Registered", data)
+  }
+
+  return(
+    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-300">
+      <h2 className="text-2xl font-bold text-black mb-6">Register with Us</h2>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="bg-slate-300 p-6 rounded-lg shadow-lg w-70">
+        <input {...register("fullName")} placeholder="Full Name" className="w-full p-2 mb-2 border rounded"/>
+
+
+      </form>
+
+
+    </div>
   )
 }
-
-export default Register
